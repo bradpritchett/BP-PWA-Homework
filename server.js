1e0d9d403cde9c7977f617314d49f5c47ef125bd
Brad Pritchett <bradpritchett1@msn.com>
2020-08-20 22:31:09 -0600

    Update to server with mongod_uri and screenshot

--- server.js
+++ server.js
@@ -14,13 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-	useNewUrlParser: true,
-	useFindAndModify: false
-});
-
-
+mongoose.connect(MONGODB_URI);
 // routes
 app.use(require("./routes/api.js"));
 
